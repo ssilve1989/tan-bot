816b6050142826cb1083330fc0e624809ce21682
Steven Silvestri <steven.m.silvestri@gmail.com>
2019-06-19 07:56:15 -0600

    fix injection dependency

--- src/insults/insults.service.spec.ts
+++ src/insults/insults.service.spec.ts
@@ -2,13 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { InsultsService } from './insults.service';
 import { HttpModule } from '@nestjs/common';
 import { BotModule } from '../bot/bot.module';
+import { ConfigModule } from '../config/config.module';
 
 describe('InsultsService', () => {
   let service: InsultsService;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [BotModule, HttpModule],
+      imports: [BotModule, HttpModule, ConfigModule],
       providers: [InsultsService],
     }).compile();
     service = module.get<InsultsService>(InsultsService);
